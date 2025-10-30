6ba839ffc7775f2da4deaae18e9dda204e85ef35
vibhoarbajaj <vibhoar2001@gmail.com>
2025-10-31 00:09:19 +0530

    added boilerplate code for authorizationsdk project

---
+++ authz-sdk/src/main/java/io/github/vibhoarbajaj/authz_sdk/AuthorizationSdkApplication.java
@@ -0,0 +1,13 @@
+package io.github.vibhoarbajaj.authz_sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationSdkApplication.class, args);
+	}
+
+}
---
+++ authz-sdk/src/test/java/io/github/vibhoarbajaj/authz_sdk/AuthorizationSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vibhoarbajaj.authz_sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorizationSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
